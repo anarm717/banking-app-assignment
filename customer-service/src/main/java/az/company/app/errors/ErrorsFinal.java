68e311cb600b171367bb812825180bbdd39a076f
Anar Mammadov <anar.mammadov@sectigo.app>
2024-01-11 19:37:32 +0400

    error messages cleared

--- customer-service/src/main/java/az/company/app/errors/ErrorsFinal.java
+++ customer-service/src/main/java/az/company/app/errors/ErrorsFinal.java
@@ -4,19 +4,12 @@ import az.company.app.exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorsFinal implements ErrorResponse {
-    CANT_REMOVE_DIN("CANT_REMOVE_DIN", HttpStatus.CONFLICT, "Nazirliyin ləğvi və ya silinməsi qadağandır!!!"),
     DATA_NOT_FOUND( "DATA_NOT_FOUND", HttpStatus.NOT_FOUND, "bu id-li '{id}' '{name}' məlumat tapılmadı"),
     DATA_NOT_FOUND_LAST( "DATA_NOT_FOUND_LAST", HttpStatus.NOT_FOUND, "{message}"),
-    STAFF_UNIT_BAD_REQUEST( "STAFF_UNIT_BAD_REQUEST", HttpStatus.BAD_REQUEST, "daxil edilən parametr(lər) yanlışdır: {message}"),
+    BAD_REQUEST( "BAD_REQUEST", HttpStatus.BAD_REQUEST, "daxil edilən parametr(lər) yanlışdır: {message}"),
     INTERNAL_SERVER_ERROR( "INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR, "daxili server xətası"),
-    HAS_CHILD_ERROR("HAS_CHILD_ERROR", HttpStatus.METHOD_NOT_ALLOWED, "strukturun alt strukturları olduğu üçün silmək qadağandır!!!"),
-    STAFF_ORDER_NOS_IS_NOT_EQUAL("STAFF_ORDER_NOS_IS_NOT_EQUAL", HttpStatus.BAD_REQUEST, "ştatların sıra nömrəsi düzgün deyil"),
-    MAIN_STAFF_UNIT_ERROR("MAIN_STAFF_UNIT_ERROR", HttpStatus.BAD_REQUEST, "{message}"),
     SERVICE_TYPE_ERROR("SERVICE_TYPE_ERROR", HttpStatus.BAD_REQUEST, "{message}"),
-    CHILD_HAS_STAFF("CHILD_HAS_STAFF", HttpStatus.BAD_REQUEST, "{name} bağlı olduğu ştat olduğu üçün ilk öncə ştatlarda dəyişiklik edin!!!"),
-    CHECK_EMPLOYEE_DURING_DELETE_STAFF("CHECK_EMPLOYEE_DURING_DELETE_STAFF", HttpStatus.BAD_REQUEST, "ştata bağlı işçilər var"),
-    CHECK_EMPLOYEE_DELETE_NOT_ENOUGH_STAFF("CHECK_EMPLOYEE_DELETE_NOT_ENOUGH_STAFF", HttpStatus.BAD_REQUEST, "yetərincə işçisi boş ştat yoxdur"),
-    STAFF_PARAM_NAME_IN_USE("STAFF_PARAM_NAME_IN_USE", HttpStatus.BAD_REQUEST, "parametrin istifadə edildiyi dəyər mövcuddur"),
+    PARAM_IN_USE("PARAM_IN_USE", HttpStatus.BAD_REQUEST, "parametrin istifadə edildiyi dəyər mövcuddur"),
     
     
     GSM_NUMBER_EXISTS( "GSM_NUMBER_EXISTS", HttpStatus.BAD_REQUEST, "GSM Number exist: '{gsmNumber}'"),
@@ -27,14 +20,10 @@ public enum ErrorsFinal implements ErrorResponse {
     //pdf generation exception messages
     GENERATE_PDF_EXCEPTION( "GENERATE_PDF_EXCEPTION", HttpStatus.INTERNAL_SERVER_ERROR, "{message}"),
 
-    //get staff unit attributes
-    STAFF_UNIT_GET_ATTRIBUTES("STAFF_UNIT_GET_ATTRIBUTES", HttpStatus.INTERNAL_SERVER_ERROR, "{message}"),
-
     //for testing exceptions
     EXCEPTION_IN_TESTING("EXCEPTION_IN_TESTING", HttpStatus.INTERNAL_SERVER_ERROR, "testing zamanı xəta yarandı"),
 
     //security exception messages
-//    JWT_ERROR("JWT_ERROR",HttpStatus.UNAUTHORIZED,"{message}"),
     ACCESS_DENIED("ACCESS_DENIED",HttpStatus.FORBIDDEN,"İcazə yoxdur"),
     EXPIRED_JWT_ERROR("EXPIRED_JWT_ERROR", HttpStatus.UNAUTHORIZED, "JWT token-in vaxtı keçib"),
     UNSUPPORTED_JWT_ERROR("UNSUPPORTED_JWT_ERROR",HttpStatus.UNAUTHORIZED,"Bu token formatı dəstəklənmir"),
--- payment-service/src/main/java/az/company/app/errors/ErrorsFinal.java
+++ payment-service/src/main/java/az/company/app/errors/ErrorsFinal.java
@@ -4,19 +4,12 @@ import az.company.app.exception.ErrorResponse;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorsFinal implements ErrorResponse {
-    CANT_REMOVE_DIN("CANT_REMOVE_DIN", HttpStatus.CONFLICT, "Nazirliyin ləğvi və ya silinməsi qadağandır!!!"),
     DATA_NOT_FOUND( "DATA_NOT_FOUND", HttpStatus.NOT_FOUND, "bu id-li '{id}' '{name}' məlumat tapılmadı"),
     DATA_NOT_FOUND_LAST( "DATA_NOT_FOUND_LAST", HttpStatus.NOT_FOUND, "{message}"),
-    STAFF_UNIT_BAD_REQUEST( "STAFF_UNIT_BAD_REQUEST", HttpStatus.BAD_REQUEST, "daxil edilən parametr(lər) yanlışdır: {message}"),
+    BAD_REQUEST( "BAD_REQUEST", HttpStatus.BAD_REQUEST, "daxil edilən parametr(lər) yanlışdır: {message}"),
     INTERNAL_SERVER_ERROR( "INTERNAL_SERVER_ERROR", HttpStatus.INTERNAL_SERVER_ERROR, "daxili server xətası"),
-    HAS_CHILD_ERROR("HAS_CHILD_ERROR", HttpStatus.METHOD_NOT_ALLOWED, "strukturun alt strukturları olduğu üçün silmək qadağandır!!!"),
-    STAFF_ORDER_NOS_IS_NOT_EQUAL("STAFF_ORDER_NOS_IS_NOT_EQUAL", HttpStatus.BAD_REQUEST, "ştatların sıra nömrəsi düzgün deyil"),
-    MAIN_STAFF_UNIT_ERROR("MAIN_STAFF_UNIT_ERROR", HttpStatus.BAD_REQUEST, "{message}"),
     SERVICE_TYPE_ERROR("SERVICE_TYPE_ERROR", HttpStatus.BAD_REQUEST, "{message}"),
-    CHILD_HAS_STAFF("CHILD_HAS_STAFF", HttpStatus.BAD_REQUEST, "{name} bağlı olduğu ştat olduğu üçün ilk öncə ştatlarda dəyişiklik edin!!!"),
-    CHECK_EMPLOYEE_DURING_DELETE_STAFF("CHECK_EMPLOYEE_DURING_DELETE_STAFF", HttpStatus.BAD_REQUEST, "ştata bağlı işçilər var"),
-    CHECK_EMPLOYEE_DELETE_NOT_ENOUGH_STAFF("CHECK_EMPLOYEE_DELETE_NOT_ENOUGH_STAFF", HttpStatus.BAD_REQUEST, "yetərincə işçisi boş ştat yoxdur"),
-    STAFF_PARAM_NAME_IN_USE("STAFF_PARAM_NAME_IN_USE", HttpStatus.BAD_REQUEST, "parametrin istifadə edildiyi dəyər mövcuddur"),
+    PARAM_IN_USE("PARAM_IN_USE", HttpStatus.BAD_REQUEST, "parametrin istifadə edildiyi dəyər mövcuddur"),
 
     //pdf generation exception messages
     GENERATE_PDF_EXCEPTION( "GENERATE_PDF_EXCEPTION", HttpStatus.INTERNAL_SERVER_ERROR, "{message}"),
@@ -27,9 +20,6 @@ public enum ErrorsFinal implements ErrorResponse {
     
     GSM_NUMBER_NOT_FOUND( "GSM_NUMBER_NOT_FOUND", HttpStatus.NOT_FOUND, "GSM Number not found : '{gsmNumber}'"),
     CUSTOMER_SERVICE_ERROR( "CUSTOMER_SERVICE_ERROR", HttpStatus.CONFLICT, "Customer Service Error"),
-
-    //get staff unit attributes
-    STAFF_UNIT_GET_ATTRIBUTES("STAFF_UNIT_GET_ATTRIBUTES", HttpStatus.INTERNAL_SERVER_ERROR, "{message}"),
 
     //for testing exceptions
     EXCEPTION_IN_TESTING("EXCEPTION_IN_TESTING", HttpStatus.INTERNAL_SERVER_ERROR, "testing zamanı xəta yarandı"),
